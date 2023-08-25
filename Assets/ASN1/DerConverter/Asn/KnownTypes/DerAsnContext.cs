e1e6de88659d247846992164c89d8a5f343d6ba7
willneedit <willneedit@nurfuerspam.de>
2023-08-25 13:56:45 +0200

    DerAsnContext to ecapsulate ContextSpecific tags

---
+++ Assets/ASN1/DerConverter/Asn/KnownTypes/DerAsnContext.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace DerConverter.Asn.KnownTypes
+{
+    public class DerAsnContext : DerAsnType<DerAsnType>
+    {
+        public DerAsnContext(IDerAsnDecoder decoder, DerAsnIdentifier identifier, Queue<byte> rawData)  // to public 210707
+            : base(decoder, identifier, rawData)
+        {
+        }
+
+        public DerAsnContext(DerAsnIdentifier identifier, DerAsnType value)
+            : base(identifier, value)
+        {
+        }
+
+        public DerAsnContext(long tag, DerAsnType value)
+            : base(new DerAsnIdentifier(
+                DerAsnTagClass.ContextSpecific, 
+                DerAsnEncodingType.Constructed, tag), value)
+        {
+        }
+
+        protected override DerAsnType DecodeValue(IDerAsnDecoder decoder, Queue<byte> rawData) 
+            => decoder.Decode(rawData);
+
+        protected override IEnumerable<byte> EncodeValue(IDerAsnEncoder encoder, DerAsnType value) 
+            => encoder.Encode(value);
+    }
+}
